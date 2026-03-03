b2a8d2e104023fc0d6b049ad47563a4aae512d4b
Ken <PrinceKenneth.Castillo@cognizant.com>
2026-03-03 21:18:12 +0800

    Added my Routes for Home and Watchlist Funcitons

--- moviedux/src/App.jsx
+++ moviedux/src/App.jsx
@@ -1,8 +1,10 @@
 // Components:
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+
 import Header from './components/Header.jsx'
 import Footer from './components/Footer.jsx'
 import MoviesGrid from './components/MoviesGrid.jsx' 
-
+import WatchList from './components/WatchList.jsx'
 
 import './App.css'
 import './styles.css'
@@ -13,7 +15,29 @@ function App() {
 
       <div className="container">
         <Header />
-        <MoviesGrid />        
+
+        <Router>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">
+                  Home
+                </Link>
+              </li>
+
+              <li>
+                <Link to="/watchlist">
+                  Watchlist
+                </Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Routes>
+            <Route path='/' element={<MoviesGrid />}></Route>
+            <Route path='/watchlist' element={<WatchList />}></Route>
+          </Routes>
+        </Router>
       </div>
       <Footer />
     </div>
---
+++ moviedux/src/components/WatchList.jsx
@@ -0,0 +1,8 @@
+
+export default function WatchList(){
+  return <div>
+    <h1 className="title">
+      Youre Watchlist
+    </h1>
+  </div>
+}
