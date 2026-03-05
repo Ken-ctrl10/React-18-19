2e5a27329d16bc9a02511ccf969ec3b8f544da61
Ken <PrinceKenneth.Castillo@cognizant.com>
2026-03-05 13:56:02 +0800

    Creating a new project bug-blaster and setting it up

---
+++ bug-blaster/src/App.jsx
@@ -0,0 +1,17 @@
+//Importing Components
+
+
+// Importing CSS
+import './App.css'
+import './styles.css'
+
+function App() {
+
+  return <div className="App">
+    <div className="container">
+      <h1>Bug Blaster</h1>
+    </div>
+  </div>
+}
+
+export default App
