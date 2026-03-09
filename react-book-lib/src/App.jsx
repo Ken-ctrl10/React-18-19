d174c202f3c902d90bc62a022d4a85ec59822ed3
Ken <PrinceKenneth.Castillo@cognizant.com>
2026-03-09 16:56:49 +0800

    Created a project more focus on routing. useParams and applying other concepts like context

---
+++ react-book-lib/src/App.jsx
@@ -0,0 +1,25 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+
+import { BooksProvider } from './BooksContext'
+import { BookDetails } from './components/BookDetails';
+import Books from './components/Books'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <BooksProvider>
+      <Router>
+        <Routes>
+          <Route path='/' element={<Books></Books>}></Route>
+          <Route path='/books' element={<Navigate to="/"/>}></Route>
+          <Route path='/books/:bookID' element={<BookDetails />}></Route>
+        </Routes>
+      </Router>
+
+    </BooksProvider> 
+  )
+}
+
+export default App
---
+++ react-book-lib/src/components/BookDetails.jsx
@@ -0,0 +1,25 @@
+import { useContext } from 'react';
+import { BooksContext } from '../BooksContext';
+import { useParams } from 'react-router-dom';
+import Books from './Books';
+
+export function BookDetails() {
+
+  const { bookID } = useParams();
+  const books = useContext(BooksContext);
+  const book = books.find(b => b.id === parseInt(bookID));
+  
+  if (Books.length === 0) {
+    return <div>Page Loading...</div>;
+  }
+
+  if (book == null ) { // or !book
+    return <div>Book not found.</div>
+  }
+
+  return <div>
+    <h1>{book.title}</h1>
+    <h2>by {book.author}</h2>
+    <p>{book.description}</p>
+  </div>
+}
